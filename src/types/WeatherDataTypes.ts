e7256b5812fa8b2266593401b86533271e4c62cb
Oxayr <uzairmaqsood451@gmail.com>
2024-10-07 14:26:03 +0500

    added the type for the data fetch

---
+++ src/types/WeatherDataTypes.ts
@@ -0,0 +1,16 @@
+interface Coordinates{
+    lat:number;
+    lon:number;
+}
+
+interface WeatherData{
+    main: string;
+}
+
+interface GeolocationError{
+    code: number;
+    message: string;
+}
+
+
+}
